ff6f916e224536fb1f7351f47cf67dcfc029a31e
JR Blanco <jr.cosio1@gmail.com>
2021-09-17 00:28:37 +0200

    Viernes 17S

---
+++ app/src/main/java/com/jrblanco/calculadoradejoyeros2021/CalcularPlataActivity.java
@@ -0,0 +1,16 @@
+package com.jrblanco.calculadoradejoyeros2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalcularPlataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+
+        setContentView(R.layout.activity_calcular_plata);
+    }
+}
